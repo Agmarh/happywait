6ab0b18b7672fb48fa55730086cac55c974678e0
Guillaume <guillaumeguegan@outlook.fr>
2021-07-28 09:36:20 +0200

    Création d'un lien signé et d'une route protégée

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -5,23 +5,42 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use CoopTilleuls\UrlSignerBundle\UrlSigner\UrlSignerInterface;
 
 class APIController extends AbstractController
 {
+    public function __construct(UrlSignerInterface $urlSigner) {
+        $this->urlSigner = $urlSigner;
+    }
+
     /**
-     * @Route("/api/{email}", name="api")
+     * @Route("/api/send/{email}", name="api_send_email")
      */
     public function SendEmail(string $email, \Swift_Mailer $mailer): JsonResponse
     {
+        $url = $this->generateUrl('api_link');
+        // Le lien expirera après 10 minutes
+        $expiration = (new \DateTime('now'))->add(new \DateInterval('PT10M'));
+
         $message = (new \Swift_Message('Hello Email'))
         ->setFrom('send@example.com')
         ->setTo($email)
-        ->setBody("Bonjour");
+        ->setBody($this->urlSigner->sign($url, $expiration));
 
         $mailer->send($message);
 
         return $this->json([
-            'email' => $email
+            'message' => 'Mail Send !'
+        ]);
+    }
+
+    /**
+     * @Route("/api/link", name="api_signed_link", defaults={"_signed": true})
+     */
+    public function SignedLink(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Merci d\'avoir utilisé mon API !'
         ]);
     }
 }
