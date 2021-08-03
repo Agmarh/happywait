da9cbe864f24348673df6c3c81066350513c0c81
gguegan <guillaume.guegan@morbihan-energies.fr>
2021-08-03 11:56:06 +0200

    Authentification par Token

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use CoopTilleuls\UrlSignerBundle\UrlSigner\UrlSignerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 class APIController extends AbstractController
@@ -15,19 +16,19 @@ class APIController extends AbstractController
     }
 
     /**
-     * @Route("/api/send/{email}", name="api_send_email", methods={"GET"})
+     * @Route("/send/{email}", name="api_send_email", methods={"GET"})
      */
     public function SendEmail(string $email, \Swift_Mailer $mailer): JsonResponse
-    {
+    {   
         // Récupération de la Request
         $request = Request::createFromGlobals();
         
         // Création du lien signé
-        $url = $this->generateUrl('api_signed_link');        
+        $url = $this->generateUrl('api_signed_link', ['email' => $email]);        
         $expiration = (new \DateTime('now'))->add(new \DateInterval('PT10M')); // Le lien expirera après 10 minutes
 
         // Création du mail
-        $message = (new \Swift_Message('Hello Email'))
+        $message = (new \Swift_Message('Authentification via Token'))
         ->setFrom('send@example.com')
         ->setTo($email)
         ->setBody($request->getSchemeAndHttpHost() . $this->urlSigner->sign($url, $expiration));
@@ -40,14 +41,27 @@ class APIController extends AbstractController
     }
 
     /**
-     * @Route("/api/link", name="api_signed_link", defaults={"_signed": true})
+     * @Route("/link", name="api_signed_link", defaults={"_signed": true})
      * 
      * _signed : paramètre de UrlSignerBundle pour sécuriser la route
      */
-    public function SignedLink(): JsonResponse
-    {
+    public function SignedLink(Request $request, JWTEncoderInterface $JWTEncoder): JsonResponse
+    {      
         return $this->json([
-            'message' => 'Merci Happywait d\'avoir utilisé mon API !'
+            'token' => $JWTEncoder->encode([
+                'username' => $request->query->get('email'),
+                'exp' => time() + 7200 // 2 hour expiration
+            ])
+        ]);
+    }
+
+    /**
+     * @Route("/api", name="api_test_token")
+     * */
+    public function API(): JsonResponse
+    {      
+        return $this->json([
+            'message' => "Merci pour ce test ! Au plaisir !"
         ]);
     }
 }
