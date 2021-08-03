f05229e96f0be70292061221e288635353b2614e
gguegan <guillaume.guegan@morbihan-energies.fr>
2021-08-03 12:52:21 +0200

    Configuration Doctrine ORM

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,7 @@ return [
     CoopTilleuls\UrlSignerBundle\CoopTilleulsUrlSignerBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
